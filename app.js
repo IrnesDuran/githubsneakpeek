ecbe12ac7a83467a420b4069a3e0d7749e084d83
darkhitect1985 <55331764+darkhitect1985@users.noreply.github.com>
2019-12-14 21:46:36 +0100

    ver1

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //Init github
 const github = new Github;
+//init UI
+const ui = new UI;
 
 //search input
 const searchUser = document.getElementById('searchUser');
@@ -15,14 +17,17 @@ if (userText !== '') {
     github.getUser(userText).then(data => {
         if (data.profile.message ==='Not Found') {
             //show an alert that user has not been found
+            ui.showAlert('User not found','alert alert-danger');
 
         } else {
-            //SHow profile
-
+            //Show profile
+            ui.showProfile(data.profile);
+            ui.showRepos(data.repos);
         }
     })
 } else {
     // clear profile
+    ui.clearProfile();
 };
 
 })
--- github.js
+++ github.js
@@ -2,15 +2,22 @@ class Github {
     constructor() {
       this.client_id = '23128d14bf59fac42c4b';
       this.client_secret = 'c37ad31f6e56268caefbab0d28b91dfa1610712c';
+      this.repos_count = 5;
+      this.repos_sort = 'created: asc'
     }
   
     async getUser(user) {
       const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+      const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
   
       const profile = await profileResponse.json();
-  
+
+      const repos = await repoResponse.json();
+
       return {
-        profile
+        profile,
+        repos
       }
     }
   }
